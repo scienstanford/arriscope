a6a383f0fa775cdf81d961e91755690348168c20
Brian Wandell <wandell@stanford.edu>
2020-05-02 16:33:00 -0700

    Made the matrix

--- scripts/contrast/s_arriContrastChart.m
+++ scripts/contrast/s_arriContrastChart.m
@@ -8,31 +8,21 @@
 
 %%
 ieInit
+
 %% lights
 
 Lights = {'whiteARRILight.mat',  'whiteSonyLight.mat','greenSonyLight.mat',...
     'blueSonyLight.mat','redSonyLight.mat','violetSonyLight.mat'};
+nLights = numel(Lights);
 
-% for ii = 1:length(Lights)
-%     % loop when we are ready
-% end
-ii = 1;
+%% Default scene
 
-%% Default usage with sceneCreate
-
-scene = sceneCreate('reflectance chart');
-sceneWindow(scene);
-
-%% Create your own chart 
-
-% The files containing the reflectances are in ISET format, readable by 
-% s = ieReadSpectra(sFiles{1});
 sFiles = cell(1,1);
 sFiles{1} = which('tissueReflectances.mat');
 % [t,w] = ieReadSpectra(sFiles{1},wave); plotReflectance(w,t(:,[3,7,8]));
 
 % The number of samples from each of the data sets, respectively
-sSamples = 11;    % 
+sSamples = 11;    %
 
 % How many row/col spatial samples in each patch (they are square)
 pSize    = 24;           % Patch size
@@ -42,97 +32,106 @@ sampling = 'no replacement';
 
 scene = sceneCreate('reflectance chart',pSize,sSamples,sFiles,wave,grayFlag,sampling);
 scene = sceneSet(scene,'name','Pig tissues');
-%% set the light
 
-wave = sceneGet(scene,'wave');
-ThisLight = ieReadSpectra(Lights{ii},wave);
-% ieNewGraphWin; plot(wave,ThisLight);
-sceneThisLight= sceneAdjustIlluminant(scene,ThisLight);
-sceneThisLight = sceneSet(sceneThisLight,'name','Reflectance Chart');
-ieAddObject(sceneThisLight); sceneWindow;
+% sceneWindow(scene);
 
-% The chart parameters are attached to the scene object
-sceneGet(sceneThisLight,'chart parameters')
-
-% Show it on the screen
-sceneWindow(sceneThisLight);
-
-%%
+%% Build the oi
 oi = oiCreate;
 oi = oiCompute(oi,scene);
 
-%%
+%% Build the sensor
 
 sensor = sensorCreate;
-fov = sceneGet(scene,'fov');
+sensor = sensorSet(sensor,'wave',wave);
+fov    = sceneGet(scene,'fov');
 sensor = sensorSetSizeToFOV(sensor,[sceneGet(scene,'hfov'),sceneGet(scene,'vfov')],scene,oi);
 
-wave = sensorGet(sensor,'wave');
-% Load sensor
 fullFileName = fullfile(arriRootPath,'data','sensor','ARRIestimatedSensors.mat');
-data = ieReadColorFilter(wave,fullFileName); 
-sensor = sensorSet(sensor,'filter spectra',data);
+arriQE = ieReadColorFilter(wave,fullFileName);
+sensor = sensorSet(sensor,'filter spectra',arriQE);
 
-sensor = sensorCompute(sensor,oi);
-sensorWindow(sensor);
+%%  We will set the parameters
 
-%%
 ip = ipCreate;
-ip = ipCompute(ip,sensor);
-ipWindow(ip);
+
+%%  Get the sensor data from all 6 lights
+
+for ii=1:nLights
+    
+    % Set the light
+    ThisLight = ieReadSpectra(Lights{ii},wave);
+    % ieNewGraphWin; plot(wave,ThisLight);
+    sceneThisLight = sceneAdjustIlluminant(scene,ThisLight);
+    % sceneThisLight = sceneSet(sceneThisLight,'name','Reflectance Chart');
+    % sceneWindow(sceneThisLight);
+    
+    % The chart parameters are attached to the scene object
+    % sceneGet(sceneThisLight,'chart parameters')
+    % sceneWindow(sceneThisLight);
+    oi     = oiCompute(oi,sceneThisLight);
+    sensor = sensorCompute(sensor,oi);
+    ip     = ipCompute(ip,sensor);
+    % ipWindow(ip);
+    
+    %%
+    if ii==1
+        rPatch = 4; cPatch = 3;
+        cp = chartCornerpoints(ip,true);
+        [rects,mLocs,pSize] = chartRectangles(cp,rPatch,cPatch,0.5);
+        rectHandles = chartRectsDraw(ip,rects);
+        delta = 7; fullData = true;
+        data = cell(rPatch*cPatch,1);
+        % delete(rectHandles);
+    end
+    
+    % Get the data and assign it
+    thisLightData = chartPatchData(ip,mLocs,delta,fullData);
+    for pp=1:numel(thisLightData)
+        data{pp} = [data{pp},thisLightData{pp}];
+    end
+    
+end
+
+%% Compute the reduced dimension of the sensor data
+
+sensorLight = zeros(numel(wave),3,numel(Lights));
+for ii = 1:numel(Lights)
+    thisLight = ieReadSpectra(Lights{ii},wave);
+    sensorLight(:,:,ii) = diag(thisLight)*arriQE;
+end
+x = reshape(sensorLight,numel(wave),nLights*3);
+
+% We want the PCA for the channel responses
+[u,s,v] = svd(x');
+nBasis = 9;
+thisBasis = u(:,1:nBasis);
+
+%{
+ieNewGraphWin; plot(wave,x);
+ieNewGraphWin; plot(cumsum(diag(s))/sum(diag(s)));
+ieNewGraphWin; plot(thisBasis);
+%}
+
+%% Could loop on the 11 surfaces
+
+mahalMatrix = zeros(11,11);
+for ss = 1:11
+    X = data{ss}*thisBasis;
+    for ii=ss:11
+        Y = data{ii}*thisBasis;
+        mahalMatrix(ss,ii) = log10(mean(mahal(X,Y)) + mean(mahal(Y,X)))/2;
+        mahalMatrix(ii,ss) = mahalMatrix(ss,ii);
+    end
+end
+imagesc(mahalMatrix); colormap(gray);
+surf(mahalMatrix); colormap(gray);
 
 %%
-rPatch = 4;
-cPatch = 3;
-cp = chartCornerpoints(ip,true);
-[rects,mLocs,pSize] = chartRectangles(cp,rPatch,cPatch,0.5);
-rectHandles = chartRectsDraw(ip,rects);
-delta = 7;
-fullData = true;
-data = chartPatchData(ip,mLocs,delta,fullData);
-delete(rectHandles);
-
-%% Compare the mahalanobis distance
-
 % Each row is a separate 3D observation
-X = data{9};
-Y = data{6};
-
+X = data{1}*thisBasis;
+Y = data{3}*thisBasis;
+    
 % Force symmetry
 disp((mean(mahal(X,Y)) + mean(mahal(Y,X)))/2)
 
-
-%% Change the illumination from the default illuminant (equal energy) to D65
-
-wave = sceneGet(scene,'wave');  d65 = ieReadSpectra('D65',wave);
-sceneD65 = sceneAdjustIlluminant(scene,d65);
-sceneD65 = sceneSet(sceneD65,'name','Reflectance Chart D65');
-ieAddObject(sceneD65); sceneWindow;
-
-%% Add a gray strip column
-
-grayStrip = 1;
-sceneGray = sceneReflectanceChart(sFiles,sSamples,pSize,wave,grayStrip);
-sceneGray = sceneSet(sceneGray,'name','Reflectance Chart EE Gray Strip');
-
-ieAddObject(sceneGray); sceneWindow;
-
-%% Store the parameters needed to make exactly the same chart
-
-[sceneOriginal, storedSamples] = sceneReflectanceChart(sFiles,sSamples,pSize);
-sceneOriginal = sceneSet(sceneOriginal,'name','Original');
-ieAddObject(sceneOriginal); sceneWindow;
-
-sceneReplica = sceneReflectanceChart(sFiles,storedSamples,pSize);
-sceneReplica = sceneSet(sceneReplica,'name','Replica');
-ieAddObject(sceneReplica); sceneWindow;
-
-%%
-%{
-cornerPoints = chartCornerpoints(scene,true);
-rPatch = 7; cPatch = 7; sFactor = 0.5;
-[rects,mLocs,pSize] = chartRectangles(cornerPoints,rPatch,cPatch,sFactor)
-rectHandles = chartRectsDraw(scene,rects);
-data = chartPatchData(scene,mLocs,5);
-
-%}
+%% END
