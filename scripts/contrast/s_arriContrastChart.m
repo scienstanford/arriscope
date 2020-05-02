54f9483eaa7022b1b127ad79697ac46c03a49762
Brian Wandell <wandell@stanford.edu>
2020-05-01 22:32:09 -0700

    Added mahalanobis distance for contrast

--- scripts/contrast/s_arriContrastChart.m
+++ scripts/contrast/s_arriContrastChart.m
@@ -20,16 +20,16 @@ sceneWindow(scene);
 % s = ieReadSpectra(sFiles{1});
 sFiles = cell(1,1);
 sFiles{1} = which('tissueReflectances.mat');
-% [t,w] = ieReadSpectra(sFiles{1},wave); plotReflectance(w,t);
+% [t,w] = ieReadSpectra(sFiles{1},wave); plotReflectance(w,t(:,[3,7,8]));
 
 % The number of samples from each of the data sets, respectively
-sSamples = 48;    % 
+sSamples = 11;    % 
 
 % How many row/col spatial samples in each patch (they are square)
 pSize    = 24;           % Patch size
 wave     = 400:10:640;   % Whatever is in the reflectance data file
-grayFlag = 0;            % No gray strip
-sampling = 'replacement';
+grayFlag = 0;            % Gray strip
+sampling = 'no replacement';
 
 scene = sceneCreate('reflectance chart',pSize,sSamples,sFiles,wave,grayFlag,sampling);
 scene = sceneSet(scene,'name','Pig tissues');
@@ -44,12 +44,40 @@ sceneWindow(scene);
 %%
 oi = oiCreate;
 oi = oiCompute(oi,scene);
+
+%%
 sensor = sensorCreate;
 fov = sceneGet(scene,'fov');
-sensor = sensorSetSizeToFOV(sensor,[fov,fov],scene,oi);
+sensor = sensorSetSizeToFOV(sensor,[sceneGet(scene,'hfov'),sceneGet(scene,'vfov')],scene,oi);
 sensor = sensorCompute(sensor,oi);
 
 sensorWindow(sensor);
+
+%%
+ip = ipCreate;
+ip = ipCompute(ip,sensor);
+ipWindow(ip);
+
+%%
+rPatch = 4;
+cPatch = 3;
+cp = chartCornerpoints(ip,true);
+[rects,mLocs,pSize] = chartRectangles(cp,rPatch,cPatch,0.5);
+rectHandles = chartRectsDraw(ip,rects);
+delta = 7;
+fullData = true;
+data = chartPatchData(ip,mLocs,delta,fullData);
+delete(rectHandles);
+
+%% Compare the mahalanobis distance
+
+% Each row is a separate 3D observation
+X = data{2};
+Y = data{3};
+
+% Force symmetry
+disp((mean(mahal(X,Y)) + mean(mahal(Y,X)))/2)
+
 
 %% Change the illumination from the default illuminant (equal energy) to D65
 
@@ -77,4 +105,11 @@ sceneReplica = sceneSet(sceneReplica,'name','Replica');
 ieAddObject(sceneReplica); sceneWindow;
 
 %%
+%{
+cornerPoints = chartCornerpoints(scene,true);
+rPatch = 7; cPatch = 7; sFactor = 0.5;
+[rects,mLocs,pSize] = chartRectangles(cornerPoints,rPatch,cPatch,sFactor)
+rectHandles = chartRectsDraw(scene,rects);
+data = chartPatchData(scene,mLocs,5);
 
+%}
