06f15e64a3d5d469c028ff0d1ba264141f710817
Joyce Farrell <joyce@Joyces-iMac-3.local>
2022-02-11 11:44:12 -0800

    comparing RGB values predicted by sensors to RGB values measured
    this script compares the RGB values for 24 surfaces illuminated by 6 lights predicted by different sensors to the measured RGB values

--- scripts/sensors/s_deriveSensors.m
+++ scripts/sensors/s_deriveSensors.m
@@ -28,7 +28,14 @@
 %
 % 
 % see ARRIestimatedSensorsNoNIRfilter
-
+%% TODO
+% will need to clean up the many different sensor QE functions we have in
+% data/sesnor
+% For example 
+% ARRISensorNIRoff.mat is the Sony sensor functions - not estimated
+% ARRISensorNIRon.mat is the estimated ARRI sensor functions - based on
+%
+% will need to save ScaledSony and FilteredAndScaledSony 
 %% set the Matlab path
 %{
 cd /users/joyce/GitHub/isetcam/;
@@ -64,6 +71,8 @@ plot(wave,scaledSonyR,'r'); hold on;
 plot(wave,scaledSonyG,'g');
 plot(wave,scaledSonyB,'b');
 
+ScaledSony = [scaledSonyR,scaledSonyG,scaledSonyB]; % save this if we like it.
+
 % perhaps we should save these as scaled Sony sensor QE functions
 %{
 sensor = [scaledSonyR, scaledSonyG, scaledSonyB];
@@ -96,6 +105,13 @@ ieNewGraphWin;
 plot(wave,filteredSonyR,'r'); hold on;
 plot(wave,filteredSonyG ,'g');
 plot(wave,filteredSonyB,'b');
+FilteredAndScaledSony = [filteredSonyR,filteredSonyG,filteredSonyB];
+%{
+wavelength = 400:10:900;
+data = FilteredAndScaledSony;
+comment = 'These are the QE functions that have been scaled to match the RGB gains estimated for the ARRIscope (see s_arriSensorEstimation.m) and filtered using the UVandIR filter that we think is a good fit for the ARRIscope filter'
+ieSaveSpectralFile(wavelength,data,comment);
+%}
 
 %% Filter the scaled OnSemi QE functions using a UVandIR filter
 % Perhaps we will see how well these functions do at predicting the
@@ -111,27 +127,138 @@ plot(wave,filteredOnSemiG ,'g--');
 plot(wave,filteredOnSemiB,'b--');
 %}
 
-%%
+%% 
+% See how well the scaled and filtered Sony QE functions do at predicting the 24 patches illuminated by the 6 spectral lights
+%% measured RGB values
+% Get the mean RGB values for the MCC chart illuminated by 6 lights
+% Display the raw camera images captured by the ARRIScope camera with the
+% NIR filter on for each of the 6 lights
+% Then grab the mean R, G and B values for each of the 24 patches captured
+% under each of the 6 lights
+% Notice that the raw camera image captured under violet17 has very little
+% signal and is, therefore, noisy
+chdir(fullfile(arriRootPath,'data','macbethColorChecker','MacbethIRON'));
+rgbImages = {'MacbethCc_blue17_fIRon.ari','MacbethCc_green17_fIRon.ari', ...
+    'MacbethCc_red17_fIRon.ari', 'MacbethCc_violet17_fIRon.ari', ...
+    'MacbethCc_white17_fIRon.ari','MacbethCc_arriwhite20_fIRon.ari'};
+ip = ipCreate;
+ip = ipSet(ip,'correction method illuminant','none');
+ip = ipSet(ip,'conversion method sensor','none');
+%{
+ img = arriRead(rgbImages{end},'image','left');
+ ieNewGraphWin;
+ imagescRGB(img);
+%}
+%{
+% Make the display look right for the blue case.  What's going on?
+ img = arriRead(rgbImages{end},'image','left');
+ img = imresize(img,1/4);
+ ip = ipSet(ip,'result',img);
+ ipWindow(ip);
+ showSelection = true;
+ fullData = false;
+ [thisRGB,~,~,cornerPoints] = macbethSelect(ip,showSelection,fullData);
+%}
 
+showSelection = true;   % Do or do not bring up the window
+fullData      = false;  % Just returns the mean in each patch
+cornerPoints = [
+    79   291;
+   490   292;
+   489    19;
+    79    22];
+
+mRGB = [];
+for ii=1:numel(rgbImages)
+    img = arriRead(rgbImages{ii},'image','left');
+    img = imresize(img,1/4);
+    ip  = ipSet(ip,'result',img); 
+    thisRGB = macbethSelect(ip,showSelection,fullData,cornerPoints);
+    mRGB = [mRGB; thisRGB];
+end
+
+%% Predicted RGB values
+wave = 400:10:700;
+% Load the macbeth reflectances
+surfaces = ieReadSpectra('MiniatureMacbethChart.mat',wave);
+plotRadiance(wave,surfaces);
+% For each light multiply each MCC patch to get the expected radiance
+testLights = {'blueSonyLight.mat','greenSonyLight.mat',...
+    'redSonyLight.mat','violetSonyLight.mat',...
+    'whiteSonyLight.mat','whiteARRILight.mat'};  
+
+radiance = [];
+for ii=1:numel(testLights)
+    thisLight = ieReadSpectra(testLights{ii},wave);
+    radiance = [radiance, diag(thisLight(:))*surfaces];
+end
+
+%{
+plotRadiance(wave,radiance);
+title('MCC under 6 different lights')
+%}
+colorList1 = {'r-','g-','b-'};
+colorList2 = {'ro','go','bo'};
+sensorQE = ieReadSpectra('FilteredAndScaledSonyQE.mat',wave);
+ieNewGraphWin;
+estimatedFiltersRGB = sensorQE'*radiance;
+estimatedFiltersRGB = estimatedFiltersRGB';
+for ii=1:3
+    plot(ieScale(estimatedFiltersRGB(:,ii),1),ieScale(mRGB(:,ii),1),colorList2{ii});
+    hold on;
+end
+identityLine;
+xlabel('RGB values predicted by the scaled and Filtered Sony QE');
+ylabel('RGB values measured by ARRI sensors');
+
+%% Compare to the scaled TLC QE functions
 % TLCI are QE functions for a standard color camera model
 % We found that the scaled TLCI QE functions predict the ARRIscope RGB values (measured 
 % for the 24 patches illuminated by the 6 spectral lights) just as well as
 % the QE functions we estimated 
 % Here we compare the scaled TLCI QE functions to the scaled and filtered
 % Sony QE functions
+% TLCISensorFname = fullfile(arriRootPath,'data','sensor','TLCIsensors.mat');
+% TLCIQE = ieReadSpectra(TLCISensorFname, wave);
+% ScaledTLCIr = TLCIQE(:,1)/max(TLCIQE(:,1))* max(arriQE(:,1));
+% ScaledTLCIg = TLCIQE(:,2)/max(TLCIQE(:,2))* max(arriQE(:,2));
+% ScaledTLCIb = TLCIQE(:,3)/max(TLCIQE(:,3))* max(arriQE(:,3));
+%{
+ieNewGraphWin;
+plot(wave,ScaledTLCIr,'r--'); hold on
+plot(wave,ScaledTLCIg,'g--');
+plot(wave,ScaledTLCIb,'b--');
+%}
+%{
+wavelength = 400:10:700;
+data = [ScaledTLCIr,ScaledTLCIg,ScaledTLCIb];
+comment = 'TLCI QE functions scaled to the RGB gains estimated for the ARRIScope';
+ieSaveSpectralFile(wavelength, data, comment);
+%}
 
-plot(wave,TLCIQE(:,1)/max(TLCIQE(:,1))* max(arriQE(:,1)),'r--'); hold on
-plot(wave,TLCIQE(:,2)/max(TLCIQE(:,2))* max(arriQE(:,2)),'g--');
-plot(wave,TLCIQE(:,3)/max(TLCIQE(:,3))* max(arriQE(:,3)),'b--');
+sensorQE = ieReadSpectra('ScaledTLCIsensors.mat',wave);
+ieNewGraphWin;
+estimatedFiltersRGB = sensorQE'*radiance;
+estimatedFiltersRGB = estimatedFiltersRGB';
+for ii=1:3
+    plot(ieScale(estimatedFiltersRGB(:,ii),1),ieScale(mRGB(:,ii),1),colorList2{ii});
+    hold on;
+end
+identityLine;
+xlabel('RGB values predicted by the scaled TLCI sensors');
+ylabel('RGB values measured by ARRI sensors');
 
-%%
-% will need to clean up the many different sensor QE functions we have in
-% data/sesnor
-% For example 
-% ARRISensorNIRoff.mat is the Sony sensor functions - not estimated
-% ARRISensorNIRon.mat is the estimated ARRI sensor functions - based on
-
-%% 
-% See how well the scaled and filtered Sony QE functions do at predicting the 24 patches illuminated by the 6 spectral lights
+%% Compare to the estimated Sensors
+estimatedFilters = ieReadSpectra('ARRIestimatedSensors.mat')
+ieNewGraphWin;
+estimatedFiltersRGB = estimatedFilters'*radiance;
+estimatedFiltersRGB = estimatedFiltersRGB';
+for ii=1:3
+    plot(ieScale(estimatedFiltersRGB(:,ii),1),ieScale(mRGB(:,ii),1),colorList2{ii});
+    hold on;
+end
+identityLine;
+xlabel('RGB values predicted by the estimated sensors');
+ylabel('RGB values measured by ARRI sensors');
 
 
